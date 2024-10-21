bfc1d2f163274ebf10949fd0ed4d97c4d98897a3
unknown <evikody06@gmail.com>
2024-10-21 12:29:28 +0800

    Define a function that assess if the name is valid by checking for alphabetic characters only

--- oldest_person_finder/oldest_person_finder.py
+++ oldest_person_finder/oldest_person_finder.py
@@ -1,3 +1,7 @@
-# Ask the user to input their name and age
-name = input("Please enter your name: ")
-age = int(input("PLease enter your age: "))
+def valid_name(user_name): # Define a function named "valid_name" to validate each names inputted
+    # Use .isalpha to check if "user_name" contains only alphabetic letters
+    return user_name.isalpha() # Returns True if all characters in the string are in the alphabet
+
+# Ask the user to input their name and age.
+user_name = input("Please enter your name: ")
+user_age = int(input("PLease enter your age: "))
