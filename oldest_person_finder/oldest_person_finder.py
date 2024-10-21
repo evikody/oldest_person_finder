5a45da2da7bf7a194b883fbd8f2d68fc23a66667
unknown <evikody06@gmail.com>
2024-10-21 13:00:35 +0800

    A loop that will continue asking for input until the user enters a valid name

--- oldest_person_finder/oldest_person_finder.py
+++ oldest_person_finder/oldest_person_finder.py
@@ -2,6 +2,14 @@ def valid_name(user_name): # Define a function named "valid_name" to validate ea
     # Use .isalpha to check if "user_name" contains only alphabetic letters
     return user_name.isalpha() # Returns True if all characters in the string are in the alphabet
 
-# Ask the user to input their name and age.
-user_name = input("Please enter your name: ")
-user_age = int(input("PLease enter your age: "))
+# Loop 1: This will continue asking for input until the user enters a valid name
+while True:
+        user_name = input("Please enter your name: ") # Ask the user to input their name
+
+        if valid_name(user_name): #If valid, it breaks out of the loop and moves on to the next part of the program
+            break
+        else: #If invalid, it prints an error message and repeats the process
+            print("Error: Please enter a valid name.")
+
+user_name = input("Please enter your age: ") # Ask the user to input their age
+
